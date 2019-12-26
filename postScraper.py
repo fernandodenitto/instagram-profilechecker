f094738adf0fa25690bbe48d44079991c1c1fac8
whirran <39293621+whirran@users.noreply.github.com>
2019-12-26 16:20:47 +0100

    aggiunto postScraper

---
+++ postScraper.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import json
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from igramscraper.instagram import Instagram
+
+from time import sleep
