34552d45683d1e8eadee6eefac7671de46fed179
whirran <39293621+whirran@users.noreply.github.com>
2020-02-23 10:11:30 +0100

    Data correction, aggiunti post mancanti

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -20,114 +20,147 @@ def emptyPicProfileFromURL(picProfileURL):
 def addItems(users, data, real):
 
     for user in users:
-        item = {}
-        username = user["username"]
-        # Profile picture present
-        item["profile_pic"] = False if emptyPicProfileFromURL(user["profile_pic_url"]) else True
-        # Biography present
-        item["biography"] = False if user["biography"] == "" else True
-        # Follows count
-        item["follows_count"] = user["follows_count"]
-        # Followed by count
-        item["followed_by_count"] = user["followed_by_count"]
-        # Follows count
-        item["follows_count"] = user["follows_count"]
-        # Follower/Followed ratio
-        if item["follows_count"] == 0:
-            item["ff_ratio"] = item["followed_by_count"]/(item["follows_count"] + 1)
-        else:
-            item["ff_ratio"] = item["followed_by_count"]/(item["follows_count"])
-        
-        # Media count
-        item["media_count"] = user["media_count"]
-        # Private
-        item["is_private"] = user["is_private"]
-        # Verified
-        item["is_verified"] = user["is_verified"]
-        # Is Business Account
-        item["is_business_account"] = user["is_business_account"]
-        # Is Joined recently
-        item["is_joined_recently"] = user["is_joined_recently"]
-        # Highlight reel count
-        item["highlight_reel_count"] = user["highlight_reel_count"]
-        # Connected Facebook Page
-        #item["connected_fb_page"] = False if user["connected_fb_page"] == None else True
+        if user["username"] not in ignoreList:
+            item = {}
+            username = user["username"]
+            item["username"] = user["username"]
+            # Profile picture present
+            item["profile_pic"] = False if emptyPicProfileFromURL(user["profile_pic_url"]) else True
+            # Biography present
+            item["biography"] = 0 if user["biography"] == "" else len(user["biography"])
+            # Follows count
+            item["follows_count"] = user["follows_count"]
+            # Followed by count
+            item["followed_by_count"] = user["followed_by_count"]
+            # Follows count
+            item["follows_count"] = user["follows_count"]
+            # Follower/Followed ratio
+            if item["follows_count"] == 0:
+                item["ff_ratio"] = item["followed_by_count"]/(item["follows_count"] + 1)
+            else:
+                item["ff_ratio"] = item["followed_by_count"]/(item["follows_count"])
+            
+            # Media count
+            item["media_count"] = user["media_count"]
+            # Private
+            item["is_private"] = user["is_private"]
+            # Verified
+            item["is_verified"] = user["is_verified"]
+            # Is Business Account
+            item["is_business_account"] = user["is_business_account"]
+            # Is Joined recently
+            item["is_joined_recently"] = user["is_joined_recently"]
+            # Highlight reel count
+            item["highlight_reel_count"] = user["highlight_reel_count"]
+            # Connected Facebook Page
+            #item["connected_fb_page"] = False if user["connected_fb_page"] == None else True
+            # Recently removed
+            item["recently_removed"] = True if user["username"] in removedList else False
 
-        if real==True:
-            directory = "real_posts/"
-        else:
-            directory = "fake_posts/"
+            if real==True:
+                directory = "real_posts/"
+            else:
+                directory = "fake_posts/"
+            
+            if user["username"] in removedList:
+                item["average_likes"] = 0
+                item["max_likes"] = 0
+                item["min_likes"] = 0            
+                item["std_likes"] = 0
+                item["var_likes"] = 0
+                item["skw_likes"] = 0
 
-        if os.path.isfile("users_posts/"+directory + username + ".json"):
-            with open("users_posts/"+directory + username + ".json", encoding="utf8") as t:
-                users_posts = json.load(t)
-                comments = []
-                likes = []
-                dates = []
-                videos = 0
+                item["average_comments"] = 0
+                item["max_comments"] = 0
+                item["min_comments"] = 0 
+                item["std_comments"] = 0
+                item["var_comments"] = 0
+                item["skw_comments"] = 0
+
                 
-                for post in users_posts["GraphImages"]:
-                    comments.append(post["edge_media_to_comment"]["count"])
-                    likes.append(post["edge_media_preview_like"]["count"])
-                    dates.append(post["taken_at_timestamp"])
-                    if post["is_video"]:
-                        videos = videos + 1
-                item["average_likes"] = statistics.mean(likes)
-                item["max_likes"] = max(likes)
-                item["min_likes"] = min(likes)            
-                item["std_likes"] = None if len(likes) < 2 else statistics.stdev(likes)
-                item["var_likes"] = None if len(likes) < 2 else statistics.variance(likes)
-                item["skw_likes"] = skew(likes)
+                item["mean_time_between_posts"] = 0
+                item["max_time_between_posts"] = 0
+                item["min_time_between_posts"] = 0
+                item["std_time_between_posts"] = 0
+                item["var_time_between_posts"] = 0
+                item["skw_time_between_posts"] = 0
+            else:
+                if os.path.isfile("users_posts/"+directory + username + ".json"):
+                    with open("users_posts/"+directory + username + ".json", encoding="utf8") as t:
+                        users_posts = json.load(t)
+                        comments = []
+                        likes = []
+                        dates = []
+                        videos = 0
+                        
+                        for post in users_posts["GraphImages"]:
+                            comments.append(post["edge_media_to_comment"]["count"])
+                            likes.append(post["edge_media_preview_like"]["count"])
+                            dates.append(post["taken_at_timestamp"])
+                            if post["is_video"]:
+                                videos = videos + 1
+                        item["average_likes"] = statistics.mean(likes)
+                        item["max_likes"] = max(likes)
+                        item["min_likes"] = min(likes)            
+                        item["std_likes"] = 0 if len(likes) < 2 else statistics.stdev(likes)
+                        item["var_likes"] = 0 if len(likes) < 2 else statistics.variance(likes)
+                        item["skw_likes"] = skew(likes)
 
-                item["average_comments"] = statistics.mean(comments)
-                item["max_comments"] = max(comments)
-                item["min_comments"] = min(comments) 
-                item["std_comments"] = None if len(comments) < 2 else statistics.stdev(comments)
-                item["var_comments"] = None if len(comments) < 2 else statistics.variance(comments)
-                item["skw_comments"] = skew(comments)
+                        item["average_comments"] = statistics.mean(comments)
+                        item["max_comments"] = max(comments)
+                        item["min_comments"] = min(comments) 
+                        item["std_comments"] = 0 if len(comments) < 2 else statistics.stdev(comments)
+                        item["var_comments"] = 0 if len(comments) < 2 else statistics.variance(comments)
+                        item["skw_comments"] = skew(comments)
 
-                dates.reverse()
-                times = [dates[i + 1] - dates[i] for i in range(len(dates)-1)]
-                
-                item["mean_time_between_posts"] = None if len(times) == 0 else statistics.mean(times)
-                item["max_time_between_posts"] = None if len(times) == 0 else max(times)
-                item["min_time_between_posts"] = None if len(times) == 0 else min(times) 
-                item["std_time_between_posts"] = None if len(times) < 2 else statistics.stdev(times)
-                item["var_time_between_posts"] = None if len(times) < 2 else statistics.variance(times)
-                item["skw_time_between_posts"] = None if len(times) == 0 else skew(times)
-        else:
-            item["average_likes"] = None
-            item["max_likes"] = None
-            item["min_likes"] = None 
-            item["std_likes"] = None
-            item["var_likes"] = None
-            item["skw_likes"] = None
+                        dates.reverse()
+                        times = [dates[i + 1] - dates[i] for i in range(len(dates)-1)]
+                        
+                        item["mean_time_between_posts"] = 0 if len(times) == 0 else statistics.mean(times)
+                        item["max_time_between_posts"] = 0 if len(times) == 0 else max(times)
+                        item["min_time_between_posts"] = 0 if len(times) == 0 else min(times) 
+                        item["std_time_between_posts"] = 0 if len(times) < 2 else statistics.stdev(times)
+                        item["var_time_between_posts"] = 0 if len(times) < 2 else statistics.variance(times)
+                        item["skw_time_between_posts"] = 0 if len(times) == 0 else skew(times)
+                else:
+                    item["average_likes"] = None
+                    item["max_likes"] = None
+                    item["min_likes"] = None 
+                    item["std_likes"] = None
+                    item["var_likes"] = None
+                    item["skw_likes"] = None
 
-            item["average_comments"] = None
-            item["max_comments"] = None
-            item["min_comments"] = None
-            item["std_comments"] = None
-            item["var_comments"] = None
-            item["skw_comments"] = None
+                    item["average_comments"] = None
+                    item["max_comments"] = None
+                    item["min_comments"] = None
+                    item["std_comments"] = None
+                    item["var_comments"] = None
+                    item["skw_comments"] = None
 
-            item["mean_time_between_posts"] = None
-            item["max_time_between_posts"] = None
-            item["min_time_between_posts"] = None 
-            item["std_time_between_posts"] = None
-            item["var_time_between_posts"] = None
-            item["skw_time_between_posts"] = None
+                    item["mean_time_between_posts"] = None
+                    item["max_time_between_posts"] = None
+                    item["min_time_between_posts"] = None 
+                    item["std_time_between_posts"] = None
+                    item["var_time_between_posts"] = None
+                    item["skw_time_between_posts"] = None
 
-        # Real account
-        if real==True:
-            item["real_account"] = True
-        else:
-            item["real_account"] = False
-        
-        data.append(item)
+            # Real account
+            if real==True:
+                item["real_account"] = True
+            else:
+                item["real_account"] = False
+            
+            data.append(item)
 
 data = []
 data_real = []
 data_fake = []
+
+with open('usernames/removed_list.txt', 'r') as f:
+    removedList = [line.strip() for line in f]
+
+with open('usernames/ignore_list.txt', 'r') as f:
+    ignoreList = [line.strip() for line in f]
 
 #
 # Dataset of real accounts
