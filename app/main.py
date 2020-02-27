18c62f952f87b6820f97c4fbba6eea8296651de3
Fernando De Nitto <fernandodenitto@gmail.com>
2020-02-27 10:32:29 +0100

    cartella app e piccole modifiche

---
+++ app/main.py
@@ -0,0 +1,17 @@
+# NOTE:Mettere solo la logica dell'app
+# Importare le librerie nelle rispettive cartelle classification e scraper
+# e qui importare solo i file di quelle cartelle
+
+
+# logica per prendere nome utente come argomento da terminale
+
+
+
+# Logica scraping
+
+
+# Qui mi serve l'oggetto utente che coviente stile dizionario cosí poi 
+# discrimino io se è privato o meno e mi comporto di conseguenza con due
+# tipi di classificatori diversi
+
+# Logica classificatione
